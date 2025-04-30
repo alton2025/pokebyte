6bd57b1fd56b84e05043bbae6702b3657f8114d5
alton2025 <alton.matovu@plu.edu>
2025-04-29 22:00:11 -0700

    Homepage beginning
    Got a base on homage needs tweaks  for drop down and needs to be bigger but  I will work on it.

--- frontend/pokebyte-search/src/pages/Dashboard.jsx
+++ frontend/pokebyte-search/src/pages/Dashboard.jsx
@@ -1,5 +1,39 @@
+import { Box, Input, InputGroup, InputLeftElement, Icon, Flex, VStack, Image} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Flex
+      height="100vh"
+      align="center"
+      justify="center"
+      bg="gray.50"
+      px={4}
+    >
+      <VStack spacing={6}>
+        {/* Pokébyte Logo */}
+        <Image
+          src="/pokebyte-logo.png"
+          alt="Pokébyte Logo"
+          boxSize="200px"
+          objectFit="contain"
+        />
+
+        {/* Rounded Search Bar */}
+        <InputGroup maxW="400px" boxShadow="md">
+          <InputLeftElement pointerEvents="none">
+            <Icon as={SearchIcon} color="gray.400" />
+          </InputLeftElement>
+          <Input
+            type="text"
+            placeholder="Search Pokemon..."
+            borderRadius="full"
+            bg="white"
+            _placeholder={{ color: "gray.500" }}
+            _focus={{ borderColor: "blue.400", boxShadow: "outline" }}
+          />
+        </InputGroup>
+      </VStack>
+    </Flex>
+  );
 }
