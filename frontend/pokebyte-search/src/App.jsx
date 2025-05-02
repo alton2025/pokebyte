fce73de102379eb93c84e781a8cbe0fd099177ac
alton2025 <alton.matovu@plu.edu>
2025-05-01 20:42:49 -0700

    file name changed
    changed dashboard file into home

--- frontend/pokebyte-search/src/App.jsx
+++ frontend/pokebyte-search/src/App.jsx
@@ -7,7 +7,7 @@ import {
 
 // layouts and pages
 import RootLayout from './layouts/RootLayout'
-import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
 import Create from './pages/Create'
 import Profile from './pages/Profile'
 
@@ -15,7 +15,7 @@ import Profile from './pages/Profile'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Dashboard />} />
+      <Route index element={<Home />} />
       <Route path="create" element={<Create />} />
       <Route path="profile" element={<Profile />} />
     </Route>
--- frontend/pokebyte-search/src/pages/Home.jsx
+++ frontend/pokebyte-search/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Box, Input, InputGroup, InputLeftElement, Icon, Flex, VStack, Image} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
-export default function Dashboard() {
+export default function Home() {
   return (
     <Flex
       height="100vh"
